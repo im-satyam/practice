87ed76a04047dee43b68261d0f6538b22b98cd47
im-satyam <157481814+im-satyam@users.noreply.github.com>
2025-02-16 21:39:15 +0530

    Getting the current user

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   registerController,
   loginController,
+  currentUserController,
 } = require("../controllers/authController");
+const authMiddleWare = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.post("/register", registerController);
 router.post("/login", loginController);
-
+router.get("/current-user", authMiddleWare, currentUserController);
 module.exports = router;
